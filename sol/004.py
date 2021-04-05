662c58d8e9946db1d647447eabf43e300ae09ab9
Yuki Kimura <wakabame89@gmail.com>
2021-04-05 23:55:08 +0900

    solve 004

---
+++ sol/004.py
@@ -0,0 +1,12 @@
+H, W = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(H)]
+
+H_table = [sum(a) for a in A]
+W_table = [0] * W
+for a in A:
+    for w in range(W):
+        W_table[w] += a[w]
+
+for h in range(H):
+    print(*[H_table[h] + W_table[w] - A[h][w] for w in range(W)])
+    
