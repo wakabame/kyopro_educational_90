d836c20c4e62de875235a1b210d8a13fa7faf3c2
Yuki Kimura <wakabame89@gmail.com>
2021-04-16 15:32:33 +0900

    solve 015

---
+++ pysol/015.py
@@ -0,0 +1,30 @@
+N = int(input())
+
+P = 10**9 + 7
+fac = [1] + [1]
+finv = [1] + [1]
+inv = [0] + [1]
+
+for i in range(2, N+1):
+    fac += [fac[-1] * i % P]
+    inv += [P - inv[P%i] * (P // i) %P]
+    finv += [finv[-1] * inv[i] % P]
+
+
+def comb(n, k):
+    if n < 0 or k < 0 or n < k:
+        return 0
+    return fac[n] * finv[k] * finv[n-k] % P
+
+
+def solve():
+    for k in range(1, N+1):
+        ans = 0
+        a = 1
+        while a <= N:
+            ans += comb(N - (k-1) * (a-1),a)
+            ans %= P
+            a += 1
+        print(ans)
+
+solve()
