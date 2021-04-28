b00891c088d63018444a50133d38af3263ea8a4a
Yuki Kimura <wakabame89@gmail.com>
2021-04-28 15:53:16 +0900

    solve

---
+++ pysol/009.py
@@ -0,0 +1,34 @@
+from math import atan2, pi
+N = int(input())
+
+XY = []
+for _ in range(N):
+    XY += [list(map(int, input().split()))]
+
+ans = 0
+# 中点をfixして、x軸とのなす角で偏角ソート
+for i in range(N):
+    x0, y0 = XY[i]
+    thetas = []
+    for j in range(N):
+        if i == j:
+            continue
+        x1, y1 = XY[j]
+        theta = atan2(y1 - y0, x1 - x0)*180/pi
+        thetas += [theta]
+    thetas.sort()
+    for j in range(N-1):
+        thetas += [thetas[j] + 360]
+    i, j = 0, 0
+    curr = 0
+    while thetas[i] < 180:
+        theta_diff = thetas[j] - thetas[i]
+        curr = max(curr, 180 - abs(180 - theta_diff))
+        if theta_diff > 180:
+            i += 1
+        else:
+            j += 1
+    ans = max(curr, ans)
+
+
+print(ans)
---
+++ pysol/010.py
@@ -0,0 +1,16 @@
+N = int(input())
+A, B = [0], [0]
+for _ in range(N):
+    c, p = map(int, input().split())
+    if c == 1:
+        A += [A[-1] + p]
+        B += [B[-1]]
+    if c == 2:
+        A += [A[-1]]
+        B += [B[-1] + p]
+
+Q = int(input())
+
+for _ in range(Q):
+    l, r = map(int, input().split())
+    print(A[r] - A[l-1], B[r] - B[l-1])
