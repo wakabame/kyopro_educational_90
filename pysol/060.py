f4d3c7a24a26f355b81593abad65e98b6bccbfa8
Yuki Kimura <wakabame89@gmail.com>
2021-06-08 13:58:49 +0900

    solve 060

---
+++ pysol/060.py
@@ -0,0 +1,25 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+def LIS(L):
+    from bisect import bisect_left
+    ret = []
+    seq = []
+    for ai in L:
+        pos = bisect_left(seq, ai)
+        if len(seq) <= pos:
+            seq.append(ai)
+        else:
+            seq[pos] = ai
+        ret += [len(seq)]
+    return ret
+
+increase_seq = LIS(A)
+decreace_seq = LIS(A[::-1])
+
+
+ans = 0
+for i in range(N):
+    print(increase_seq[i], decreace_seq[N-1-i])
+
+print(ans)
