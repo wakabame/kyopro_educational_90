0e08c21e942d8609d2fbc9e2f827cf4271656ab1
Yuki Kimura <wakabame89@gmail.com>
2021-06-13 02:21:49 +0900

    solve 064

--- pysol/064.py
+++ pysol/064.py
@@ -1,11 +1,16 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
 diff = [A[i+1] - A[i] for i in range(N-1)]
+ans = sum([abs(d) for d in diff])
 
 for _ in range(Q):
     l, r, v = map(int, input().split())
     if l > 1:
+        ans -= abs(diff[l-2])
         diff[l-2] += v
+        ans += abs(diff[l-2])
     if r < N:
+        ans -= abs(diff[r-1])
         diff[r-1] -= v
-    print(sum([abs(d) for d in diff]))
+        ans += abs(diff[r-1])
+    print(ans)
